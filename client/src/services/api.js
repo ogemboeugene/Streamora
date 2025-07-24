bf4b2c1a443beae2c058bede9a7925360aab61b3
ogemboeugene <brianeugene851@gmail.com>
2025-07-24 23:58:44 +0300

    Remove proxy setting that was forcing localhost API calls

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 // Create axios instance with default config
 const apiClient = axios.create({
