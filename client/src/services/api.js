855e7742bc04b7339ad3e4d77312b0478fb53d24
ogemboeugene <brianeugene851@gmail.com>
2025-07-25 00:28:13 +0300

    Remove proxy setting that was forcing localhost API

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
-
+const API_BASE_URL = 'https://streamora-backend-dba8gje0aze6c4gh.uaenorth-01.azurewebsites.net/api';
 // Create axios instance with default config
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
